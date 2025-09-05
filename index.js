c3eca74eb3fa0c56ba2139863def87a659704f6b
Kholmatovdev <kholmatov.dev@gmail.com>
2025-09-05 10:33:05 +0500

    cors added

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { MongoClient } = require('mongodb')
 const fs = require('fs');
 const { v4: uuidv4 } = require('uuid'); // <
 const User = require('./models/user.js')
-
+var cors = require('cors');
 
 
 
